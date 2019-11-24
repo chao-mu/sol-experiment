765d2b7393d74149ff1c10f7b60bf56415d15cd1
Hackpoetic <chao-mu@hackpoetic.com>
2019-11-23 23:19:43 -0500

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+// Sol
+#include <sol/sol.hpp>
+
+int main() {
+    sol::state lua;
+    int x = 0;
+    lua.set_function("beep", [&x]{ ++x; });
+    lua.script("beep()");
+
+    if (x != 1) {
+        std::cerr << "hmm... wtf..." << std::endl;
+    }
+
+    sol::function beep = lua["beep"];
+    beep();
+    if (x != 2) {
+        std::cerr << "uh what?" << std::endl;
+    }
+
+    return 0;
+}
